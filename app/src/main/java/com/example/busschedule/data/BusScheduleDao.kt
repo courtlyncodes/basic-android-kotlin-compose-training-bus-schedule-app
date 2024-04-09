95de498058ffcc4011bb77a5ed9c3075134eea50
Courtlyn Boykin <courtlynb@google.com>
2024-04-09 16:03:41 -0400

    add application name

--- app/src/main/java/com/example/busschedule/data/BusScheduleDao.kt
+++ app/src/main/java/com/example/busschedule/data/BusScheduleDao.kt
@@ -6,9 +6,20 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface BusScheduleDao {
-    @Query("SELECT * FROM Schedule ORDER BY arrival_time ASC")
+    @Query(
+        """
+        SELECT * FROM schedule 
+        ORDER BY arrival_time ASC    
+        """
+    )
     fun getAllStops(): Flow<List<BusSchedule>>
 
-    @Query("SELECT * FROM Schedule WHERE stop_name = :stopName ORDER BY arrival_time ASC ")
+    @Query(
+        """
+        SELECT * FROM schedule 
+        WHERE stop_name = :stopName 
+        ORDER BY arrival_time ASC 
+        """
+    )
     fun getByStopName(stopName: String): Flow<List<BusSchedule>>
 }
